d6dcf8c62562e56c578213273a30c4fe8d672dbb
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-02 15:30:26 -0300

    modelo de  usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const {request, response} = require ("express")
+const Usuario = require("../models/usuarios")
 
 const usuariosGet = (req, res)=> {
     res.json({
@@ -6,13 +7,19 @@ const usuariosGet = (req, res)=> {
     });
   }
 
-const usuariosPost = (req = request, res = response)=> {
-   const {nombre,id,email} = req.body;
+const usuariosPost = async (req = request, res = response)=> {
+
+   const {nombre,email,password} = req.body;
+
+   const usuario = new Usuario( {nombre,email,password});
+
+  //  Guandando modelo usuario en la base de datos
+   await usuario.save();
+ 
+
     res.json({
-      msg:"peticion post-controllers",
-       nombre,
-       id,
-       email,
+      msg:"usuario creado con exito!",
+      usuario
     });
     }
 
