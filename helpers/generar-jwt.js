8d72a1096df8281e64a48f67292f1e4c9f27ca77
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-04 15:11:27 -0300

    funcion para generar el token

--- helpers/generar-jwt.js
+++ helpers/generar-jwt.js
@@ -2,6 +2,21 @@ const jwt = requiere("jsonwebtoken");
 
 const generarJWT = (userId) =>{
 
+    return new Promise((resolve, reject) =>{
+        const payload  ={userId}
+
+        jwt.sign(payload,process.env.SECRETORPRIVATEKEY,{expiresIn:"4h"},(err,token)=>{
+            if(err){
+                console.log(err);
+                reject("No se genero el token");
+            }
+            else{
+                resolve(token);
+            }
+        })
+    })
+
+
 }
 
 module.exports ={
