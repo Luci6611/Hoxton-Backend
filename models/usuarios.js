b7732acfefefdb07f0076c734f85b9539a88e692
Roman <alvarezroman150@gmail.com>
2022-08-06 00:47:04 -0300

    arreglo de comillas en los type de modelo/usuario

--- models/usuarios.js
+++ models/usuarios.js
@@ -15,9 +15,7 @@ const UsuarioSchema = Schema({
         required:[true,"la contraseña es obligatoria"]
     },
     role:{
-       
      type: String,
-   
     enum:["ADMIN_ROLE","USER_ROLE"],
     default: "USER_ROLE",
         
