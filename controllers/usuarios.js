09fd1473e79040b07cef573e5cecae7a45caaeb4
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-09 14:37:38 -0300

    arreglamos bug de rol usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -16,25 +16,22 @@ const usuariosGet = async (req = request, res)=> {
       usuarios,
     });
   }
-
-const usuariosPost = async (req = request, res = response)=> {
- 
-   const {nombre,email,password} = req.body;
-
-   const usuario = new Usuario( {nombre,email,password});
-
-const salt=bcrypt.genSaltSync();
-usuario.password =bcrypt.hashSync(password, salt);
-
-  //  Guandando  usuario en la base de datos
-   await usuario.save();
- 
-
+  const usuariosPost = async (req = request, res = response) => {
+    const { nombre, email, password, role } = req.body;
+  
+    const usuario = new Usuario({ nombre, email, password, role });
+  
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+  
+    //  Guandando  usuario en la base de datos
+    await usuario.save();
+  
     res.status(201).json({
-      msg:"usuario creado con exito!",
-      usuario
+      msg: "usuario creado con exito!",
+      usuario,
     });
-    }
+  };
 
  const usuariosPut = async (req, res)=> {
     const { id } = req.params;
