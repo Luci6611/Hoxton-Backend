c3f3b2d013ea4a0371394dda81722c1fa68d5b08
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-15 18:45:42 -0300

    agregando img a los menus

--- controllers/menus.js
+++ controllers/menus.js
@@ -77,13 +77,14 @@ res.status(201).json({
 // ACTUALIZAR MENU
 const actualizarMenu = async(req,res) =>{
     const {id} = req.params;
-    const {precio, categoria, detalle, disponible} =  req.body;
+    const {precio, categoria, detalle, disponible , img} =  req.body;
     const usuario = req.usuario._id;
     let data={
         precio,
         detalle,
         categoria,
         disponible,
+        img,
         usuario,
     };
     // VALIDANDO SI NOMBRE ESTA EN MAYUSCULA
