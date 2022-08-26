8de7fb97b9fa442289e8f28715522d813353d93d
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-26 19:30:05 -0300

    se cae servidor al hacer una peticion

--- models/server.js
+++ models/server.js
@@ -24,7 +24,7 @@ class server{
     }
 
     middleware(){
-        //cors:
+        //cors d:
         this.app.use(cors())
         // leer el body que manda el frontend
         this.app.use(express.json());
