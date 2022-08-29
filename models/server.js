863b865efa7a46d04d7c39b64d734a0059c1fe1b
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-29 16:24:31 -0300

    reiniciando api

--- models/server.js
+++ models/server.js
@@ -24,7 +24,7 @@ class server{
     }
 
     middleware(){
-        //cors d:
+        //cors :
         this.app.use(cors())
         // leer el body que manda el frontend
         this.app.use(express.json());
