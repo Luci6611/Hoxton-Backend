71e441ed4b3ab3a5762c77e732e6470690b4fb6f
luciano rivera <lucianonicolasrivera8@gmail.com>
2022-08-29 19:56:32 -0300

    cors habilitado

--- models/server.js
+++ models/server.js
@@ -1,53 +1,51 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const {dbconnection} = require("../database/config");
-class server{
-    constructor(){
-        this.app = express();
-        this.port = process.env.PORT;
-        this.usuariosEndPoint = "/api/usuarios";
-        this.authPath = "/api/auth";
-        this.categoriasPath = "/api/categorias";
-        this.menuPath = "/api/menus";
-        this.buscarPath = "/api/buscar";
-        this.pedidosPath = "/api/pedidos";
+const { dbconnection } = require("../database/config");
+class server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosEndPoint = "/api/usuarios";
+    this.authPath = "/api/auth";
+    this.categoriasPath = "/api/categorias";
+    this.menuPath = "/api/menus";
+    this.buscarPath = "/api/buscar";
+    this.pedidosPath = "/api/pedidos";
 
-        this.conectarDb ();
-        this.middleware();
-        this.routes();   
+    this.conectarDb();
+    this.middleware();
+    this.routes();
+  }
+  // conectar a base de datos
+  async conectarDb() {
+    await dbconnection();
+  }
 
-    }
-    // conectar a base de datos
-     async conectarDb(){
-        await dbconnection();
-    }
+  middleware() {
+    //cors :
+    this.app.use(cors({ origin:[ process.env.CORS_DEV_CLIENT ]}));
 
-    middleware(){
-        //cors :
-        this.app.use(cors({origin:process.env.CORS_DEV_CLIENT}))
-
-        // leer el body que manda el frontend
-        this.app.use(express.json());
-        // carpeta public
-        this.app.use(express.static("public"));
-    }
-            // RUTAS
-    routes(){
-      this.app.use(this.usuariosEndPoint,require("../routes/usuarios"));
-      this.app.use(this.categoriasPath,require("../routes/categorias"));
-      this.app.use(this.authPath,require("../routes/auth"));
-      this.app.use(this.menuPath,require("../routes/menus"));
-      this.app.use(this.buscarPath,require("../routes/buscar"));
-      this.app.use(this.pedidosPath,require("../routes/pedidos"));
-    }
-    listen(){
-        this.
-        app.listen(this.port,()=>{
-            console.log("sevidor inicializado... en el puerto",this.port);
-        })
-    }
-    
+    // leer el body que manda el frontend
+    this.app.use(express.json());
+    // carpeta public
+    this.app.use(express.static("public"));
+    console.log(process.env.CORS_DEV_CLIENT);
+  }
+  // RUTAS
+  routes() {
+    this.app.use(this.usuariosEndPoint, require("../routes/usuarios"));
+    this.app.use(this.categoriasPath, require("../routes/categorias"));
+    this.app.use(this.authPath, require("../routes/auth"));
+    this.app.use(this.menuPath, require("../routes/menus"));
+    this.app.use(this.buscarPath, require("../routes/buscar"));
+    this.app.use(this.pedidosPath, require("../routes/pedidos"));
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("sevidor inicializado... en el puerto", this.port);
+    });
+  }
 }
 
 module.exports = server;
